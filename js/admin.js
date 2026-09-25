0fd1a548915ef9e699aec1105086d0a61398d049
agill05 <agilabddul30@gmail.com>
2026-09-26 00:05:50 +0800

    update

--- js/admin.js
+++ js/admin.js
@@ -1,8 +1,8 @@
 (function () {
     "use strict";
 
-    var WEBAPP_URL = "https://script.google.com/macros/s/AKfycbztuRzf8VwANcWukQkq9imZ2dSVcsUJ4Lz2z-5CbL0696Ij4OKitCJtMll-B2fDFZCb_A/exec";
-    var REQUEST_TIMEOUT_MS = 20000;
+    var WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyUltTxAQxbyADlz_C0yC2M4YQFX95jlShWJvNuFV4k9i-APpfcj4mAu2p1TnIv5zORdA/exec";
+    var REQUEST_TIMEOUT_MS = 30000;
     var MIN_YEAR = 1900;
     var TRUE_VALUES = ["ya", "yes", "true", "1", "x"];
     var VIEW_TITLES = { login: "adminLoginTitle", list: "adminListTitle", form: "adminFormTitle" };
@@ -189,7 +189,9 @@
 
         return fetch(url, options)
             .then(function (res) {
-                return res.json();
+                return res.json().catch(function () {
+                    throw serverFail("Server balas format tak terduga. Coba lagi.");
+                });
             })
             .then(
                 function (data) {
